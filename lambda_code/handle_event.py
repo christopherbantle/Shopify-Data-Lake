f4ef491563b9407901e06f96a3c3a495ad4796ea
Christopher Bantle <christopherbantle@Christophers-MacBook-Pro.local>
2019-01-23 07:19:27 -0500

    Bug fix

--- lambda_code/handle_event.py
+++ lambda_code/handle_event.py
@@ -4,6 +4,7 @@ import hmac
 import base64
 import hashlib
 import logging
+import json
 
 
 LOGGER = logging.getLogger(__name__)
@@ -16,9 +17,12 @@ def lambda_handler(event, context):
         LOGGER.warning(event)
         return
 
-    authentic_key_digest = hmac.HMAC(os.environ['SHOPIFY_AUTHENTICATION_KEY'].encode(), event['body'].encode(),
+    json_str_payload = json.dumps(event['body'], separators=(',', ':'))
+    authentic_key_digest = hmac.HMAC(os.environ['SHOPIFY_AUTHENTICATION_KEY'].encode(),
+                                     json_str_payload.encode(),
                                      hashlib.sha256).digest()
     request_digest = base64.b64decode(event['shopify_hmac'])
+
     if not hmac.compare_digest(authentic_key_digest, request_digest):
         LOGGER.warning('Computed digest does not match value from Shopift HMAC digest header')
         LOGGER.warning(event)
@@ -27,12 +31,12 @@ def lambda_handler(event, context):
     # New line character is required so that Athena will read multiple JSON objects in a single file
     # For details, see here: https://stackoverflow.com/\
     # questions/48226472/kinesis-firehose-putting-json-objects-in-s3-without-seperator-comma/51273983#51273983
-    body = event['body'] + '\n'
+    data = json_str_payload + '\n'
     client = boto3.client('firehose')
     client.put_record(
         DeliveryStreamName=os.environ['KINESIS_FIREHOSE'],
         Record={
-            'Data': body.encode()
+            'Data': data.encode()
         }
     )
 
